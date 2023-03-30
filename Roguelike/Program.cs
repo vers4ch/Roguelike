2cb9d081762ad083fe8ff3473e9a04786a2c3184
versach <dan2004degvl@mail.ru>
2023-03-30 10:43:40 +1000

    v1.6 NORM

--- Roguelike/Program.cs
+++ Roguelike/Program.cs
@@ -11,8 +11,9 @@ namespace Program
     {
         public static void Rand(char[,] world)
         {
-            int height = 45; //min = 15
-            int width = 170; //min = 50
+            Console.CursorVisible = false;
+            int height = world.GetLength(0); //min = 15
+            int width = world.GetLength(1); //min = 50
             Random rnd = new Random();
 
             //draw WORLD with
@@ -43,12 +44,12 @@ namespace Program
 
             int cordX, cordY, Nroom, hsq, wsq;
 
-            //!!
+            //sq
             Nroom = rnd.Next(height*width/50, height*width/45);
             for (int i = 0; i < Nroom; i++)
             {
-                cordX = rnd.Next(1, width-7);
-                cordY = rnd.Next(2, height-height/6);
+                cordX = rnd.Next(1, width-8);
+                cordY = rnd.Next(2, height-height/6+2);
                 
                 hsq = rnd.Next(height/7, height/6);///!!!!
                 wsq = rnd.Next(width/21, width/11);
@@ -57,7 +58,6 @@ namespace Program
                 {
                     for (int f = 0; f < wsq-5; f++)//17, 20
                     {
-                        // world[cordY + j, cordX + f] = '▓';
                         world[cordY + j, cordX + f] = '#';
                     }
                 }
@@ -95,16 +95,14 @@ namespace Program
     {
         public static void start()
         {
-            Console.WriteLine(
-                "                                             1.Play\n\n                                             2.Exit");
-            int number = 0;
-            Console.WriteLine("                                             choice number");
+            Console.Clear();
+            Console.WriteLine("\nChoice:\n1.Play\n2.Exit");
+            int number;
             number = Convert.ToInt32(Console.ReadLine());
             if (number == 1)
             {
                 Console.Clear();
             }
-
             if (number == 2)
             {
                 Console.Clear();
@@ -146,33 +144,33 @@ namespace Program
         {
             public static void Person()
             {
-                Igor a1 = new Igor("                                            1 - Игорь", "смертельный вопрос\n");
+                Igor a1 = new Igor("1 - Игорь", "смертельный вопрос\n");
                 a1.PrintPerson();
-                Igor a2 = new Igor("                                            2 - Игорь", "супер броня\n");
+                Igor a2 = new Igor("2 - Игорь", "супер броня\n");
                 a2.PrintPerson();
                 int choice = 0;
 
                 while (choice != 1 && choice != 2)
                 {
-                    Console.WriteLine("                                             choice your person");
+                    Console.Write("\nChoice your person: ");
                     choice = Convert.ToInt32(Console.ReadLine());
                 }
 
                 if (choice == 1)
                 {
-                    Console.WriteLine("                                         you choice is Person number 1\n");
+                    Console.WriteLine("\nYou choice is Person number 1\n");
                 }
 
                 if (choice == 2)
                 {
-                    Console.WriteLine("                                             your choice is Person number 2\n");
+                    Console.WriteLine("\nYour choice is Person number 2\n");
                 }
 
                 Thread.Sleep(500);
                 int number1 = 0;
                 while (number1 != 3)
                 {
-                    Console.WriteLine("                                             If you are ready push number 3\n");
+                    Console.WriteLine("If you are ready push number 3\n");
                     number1 = Convert.ToInt32(Console.ReadLine());
                 }
 
@@ -183,8 +181,12 @@ namespace Program
                     for (int i = 0; i < 50; i++)
                     {
                         Console.Clear();
-                        Console.WriteLine("                                         now we can start the game...\n");
-                        Console.WriteLine($"                        {i*2+2}%");
+                        Console.WriteLine();
+                        if(i%2 == 0)
+                            Console.WriteLine("                       Loading..\n");
+                        else
+                            Console.WriteLine("                       Loading...\n");
+                        Console.WriteLine($"                         {i*2+2}%");
                         load[i+1] = '#';
                         for (int o = 0; o < 52; o++)
                         {
@@ -202,9 +204,8 @@ namespace Program
         {
             public static void Moves(char[,] world)
             {
-                Console.CursorVisible = false;
                 Console.SetCursorPosition(0, 0);
-                int userX = 10;
+                int userX = 8;
                 int userY = 5;
                 while (true)
                 {
@@ -217,41 +218,39 @@ namespace Program
 
                         Console.WriteLine();
                     }
-
                     Console.SetCursorPosition(userX, userY);
                     Console.Write('@');
                     ConsoleKeyInfo moveKey = Console.ReadKey();
                     switch (moveKey.Key)
                     {
                         case ConsoleKey.UpArrow:
-                            if (world[userY-1, userX] != '#')
+                            if (world[userY-1, userX] != '#' && world[userY-1, userX] != '┃' && world[userY-1, userX] != '━' && world[userY-1, userX] != '┏' && world[userY-1, userX] != '┛' && world[userY-1, userX] != '┗' && world[userY-1, userX] != '┓' )
                             {
                                 userY--;
                             }
 
                             break;
                         case ConsoleKey.DownArrow:
-                            if (world[userY+1, userX] != '#')
+                            if (world[userY+1, userX] != '#' && world[userY+1, userX] != '┃' && world[userY+1, userX] != '━' && world[userY+1, userX] != '┏' && world[userY+1, userX] != '┛' && world[userY+1, userX] != '┗' && world[userY+1, userX] != '┓')
                             {
                                 userY++;
                             }
 
                             break;
                         case ConsoleKey.LeftArrow:
-                            if (world[userY, userX-1] != '#')
+                            if (world[userY, userX-1] != '#' && world[userY, userX-1] != '┃' && world[userY, userX-1] != '━' && world[userY, userX-1] != '┏' && world[userY, userX-1] != '┛' && world[userY, userX-1] != '┗' && world[userY, userX-1] != '┓')
                             {
                                 userX--;
                             }
 
                             break;
                         case ConsoleKey.RightArrow:
-                            if (world[userY, userX+1] != '#')
+                            if (world[userY, userX+1] != '#' && world[userY, userX+1] != '┃' && world[userY, userX+1] != '━' && world[userY, userX+1] != '┏' && world[userY, userX+1] != '┛' && world[userY, userX+1] != '┗' && world[userY, userX+1] != '┓')
                             {
                                 userX++;
                             }
 
                             break;
-
                     }
 
                     Console.Clear();
@@ -266,7 +265,7 @@ namespace Program
                     startGame.start();
                     ChoicePerson.Person();
                     Map.Rand(world);
-                    Move.Moves(world);
+                    Moves(world);
                 }
             }
         }
